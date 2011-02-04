8c68a564bdbe79408695f1108ba234fbf1dad11b
Timur Vafin <timur.vafin@flatsoft.com>
2011-02-04 14:35:22 +0300

    Validates presence of  skill's description

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,5 @@
 class Skill < ActiveRecord::Base
   belongs_to :position
   belongs_to :skill_type
+  validates :description, :presence => true
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Skill do
   it { should belong_to :position }
   it { should belong_to :skill_type }
+  it { should validate_presence_of :description }
 end
