24ca26f742841b0740d08d9500ad400702a14155
Timur Vafin <timur.vafin@flatsoft.com>
2011-02-08 19:54:14 +0300

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
 [
-  'CTO',
+  'Admin',
   'HR',
 ].each do |name|
-  Role.create(:name => name)
+  role = Role.find_or_create_by_name(name)
+  puts "Role: #{name} created"
+
+  User.find_or_create_by_email("#{name.underscore}@example.com") do |u|
+    u.password = '123456'
+    u.full_name = name
+    u.confirmed_at = Time.now
+  end.roles << role
+
+  puts "User: #{name.underscore}@example.com/123456 with role: #{name} created"
 end
 
 [
@@ -11,7 +20,8 @@ end
   'Старший программист',
   'Ведущий программист',
 ].each do |name|
-  Position.create(:name => name)
+  Position.find_or_create_by_name(name)
+  puts "Position: #{name} created"
 end
 
 [
@@ -43,5 +53,6 @@ end
   'Ruby',
   'Rails',
 ].each do |name|
-  SkillType.create(:name => name)
+  SkillType.find_or_create_by_name(name)
+  puts "Skill type: #{name} created"
 end
