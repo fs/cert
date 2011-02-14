58c0e297626a4f7f9ed1ec0deb73ed885027f08d
Timur Vafin <timur.vafin@flatsoft.com>
2011-02-14 12:23:19 +0300

    Spec for unique fields

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe Position do
   it { should validate_presence_of :name }
-  # it { should validate_uniqueness_of :name }
+  
+  it 'should validate name uniqueness' do
+    Factory(:position)
+    should validate_uniqueness_of :name
+  end
 
   it { should have_many :skills }
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -2,5 +2,11 @@ require 'spec_helper'
 
 describe Role do
   it { should validate_presence_of :name }
+
+  it 'should validate name uniqueness' do
+    Factory(:role)
+    should validate_uniqueness_of :name
+  end
+  
   it { should have_and_belong_to_many :users }
 end
