d191ab9cfb56361944ae6bafd7d66eb01e572649
Timur Vafin <timur.vafin@flatsoft.com>
2011-02-17 18:44:03 +0300

    Render marks as table

--- app/controllers/certification_reports_controller.rb
+++ app/controllers/certification_reports_controller.rb
@@ -13,11 +13,15 @@ class CertificationReportsController < ApplicationController
 
   def load_certification
     @certification ||= Certification.find(params[:certification_id], :include => :position)
-    @certification_marks_by_skills ||= @certification.certification_marks.includes(:user).index_and_group_by(&:skill_id)
   end
 
   def load_skills
     @skills ||= @certification.position.skills.index_and_group_by(&:skill_type_id)
+
+    @certification_marks_by_skills ||= {}
+    @certification.certification_marks.index_and_group_by(&:skill_id).each do |skill_id, marks|
+      @certification_marks_by_skills[skill_id] ||= marks.index_by(&:user_id)
+    end
   end
 
   def authorize_certification_report!
