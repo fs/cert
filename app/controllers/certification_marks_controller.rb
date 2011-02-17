7408012dffcb98417c8b08122f0dbd1d4730f0be
Timur Vafin <timur.vafin@flatsoft.com>
2011-02-17 17:11:07 +0300

    Optimize certification marks

--- app/controllers/certification_marks_controller.rb
+++ app/controllers/certification_marks_controller.rb
@@ -15,6 +15,18 @@ class CertificationMarksController < ApplicationController
 
   private
 
+  def collection
+    @skills ||= parent.position.skills
+    @skills_by_skill_type ||= @skills.index_and_group_by(&:skill_type_id)
+
+    @certification_marks_by_skill ||= parent.certification_marks.where(:user_id => current_user.id).index_by(&:skill_id)
+    @certification_marks ||= {}
+
+    @skills.each do |skill|
+      @certification_marks[skill.id] ||= @certification_marks_by_skill[skill.id] || @certification.certification_marks.build(:skill_id => skill.id)
+    end
+  end
+
   def create_resource(object)
     if end_of_association_chain.exists?(:skill_id => params[resource_request_name][:skill_id], :user_id => current_user.id)
       object.update_attributes(params[resource_request_name])
