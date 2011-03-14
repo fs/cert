d602ff2d4811401c903c20ce7654faf969f18a6a
Timur Vafin <timur.vafin@flatsoft.com>
2011-03-14 18:16:40 +0300

    Remove paginate on skill types page

--- app/controllers/skill_types_controller.rb
+++ app/controllers/skill_types_controller.rb
@@ -4,10 +4,4 @@ class SkillTypesController < ApplicationController
   
   inherit_resources
   actions :all, :except => [:show]
-
-  protected
-
-  def collection
-    @skill_types ||= end_of_association_chain.paginate(:page => params[:page])
-  end
 end
